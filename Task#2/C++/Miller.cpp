d9ecd8583f8e0709060650989a7399fb0c72123c
Sergey Samoilov <sergey201104@gmail.com>
2025-02-18 21:10:03 +0700

    second

--- Task#2/C++/Miller.cpp
+++ Task#2/C++/Miller.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <algorithm>
 #include <set>
 #include <map>
 
 using namespace std;
 
-int canon(int n){
-    
-
-    return 0;
+int powmod(int a, int x, int mod){
+    int res = 1;
+    for (int i = 1; i <= x; i++){
+        res = res * a;
+        res = res % mod;
+    }
+    return res;
 }
 
-int main(){
+int canon(int n, vector<int> A){
     // Решето Эратосфена
-    vector<int> div{2,3,5,7,11,13,17,19};
+    vector<int> div{2, 3, 5, 7, 11, 13, 17, 19};
     vector<int> prime;
+    vector<int> check;
     int X = 500;
 
     for (int i = 2; i <= X; i++){
@@ -28,9 +34,6 @@ int main(){
         }
     }
 
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
     map<int, int> q;
     int N = n - 1;
 
@@ -50,6 +53,55 @@ int main(){
     }
 
     for (auto i : q) {
-        cout << i.first << " : " << i.second << endl;
+        for (int j = 0; j < A.size(); j++){
+            int res = (powmod(j, ((n - 1)/i.first), n));
+            check.push_back(res);
+            if (res != 1){
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < check.size(); i++){
+        if (check[i] != 1){
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+    int n, t;
+    srand(time(0));
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << "Enter t: ";
+    cin >> t;
+
+    vector<int> A;
+
+    for (int i = 0; i < t; i++){
+        A.push_back(rand() % (n - 2) + 2);
+    }
+
+    int k = 0;
+    for (int i = 0; i < A.size(); i++){
+        int res = powmod(A[i], n - 1, n);
+        k += res;
+    }
+
+    if (A.size() != k){
+        cout << "n - составное!" << endl;
+        return 0;
+    }
+
+    int res = canon(n, A);
+
+    if (res == 1) {
+        cout << "n - простое!" << endl;
+    }
+    else {
+        cout << "n - составное!" << endl;
     }
 }
