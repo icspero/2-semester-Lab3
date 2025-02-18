3b99e7723c2148f49f6459ccbec665587be85819
Sergey <sergey201104@gmail.com>
2025-02-19 02:30:59 +0700

    miller_1

--- Task#2/C++/Miller.cpp
+++ Task#2/C++/Miller.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <algorithm>
 #include <set>
+#include <cmath>
 #include <map>
 
 using namespace std;
@@ -21,7 +22,7 @@ int canon(int n, vector<int> A){
     // Решето Эратосфена
     vector<int> div{2, 3, 5, 7, 11, 13, 17, 19};
     vector<int> prime;
-    vector<int> check;
+    
     int X = 500;
 
     for (int i = 2; i <= X; i++){
@@ -33,7 +34,8 @@ int canon(int n, vector<int> A){
             prime.erase(remove_if(prime.begin(), prime.end(), [&div, &i](int j) { return j % div[i] == 0 && j != div[i]; }), prime.end());
         }
     }
-
+    
+    vector<int> check;
     map<int, int> q;
     int N = n - 1;
 
@@ -71,13 +73,7 @@ int canon(int n, vector<int> A){
     return 0;
 }
 
-int main(){
-    int n, t;
-    srand(time(0));
-    cout << "Enter the number: ";
-    cin >> n;
-    cout << "Enter t: ";
-    cin >> t;
+int test(int n, int t){
 
     vector<int> A;
 
@@ -104,4 +100,73 @@ int main(){
     else {
         cout << "n - составное!" << endl;
     }
+
+    return 0;
+}
+
+int generate_m(const vector<int>& prime, int bit) {
+    srand(time(0));
+
+    int max = pow(2, bit) - 1; // максимальное значение для n
+    int min = pow(2, bit - 1); // минимальное значение для n
+    int m_max = (min - 1) / 2; // максимальное значение для m, учитывая n = 2m + 1
+
+    bool flag = false;
+    int m;
+
+    while (!flag) {
+        int temp = 1;
+        
+        // Генерация числа m
+        for (int i = 0; i < bit / 2; i++) {
+            int alpha = rand() % (bit / 2) + 1;  // случайная степень
+            for (int j = 0; j < alpha; j++) {
+                temp *= prime[i];  // возведение простого числа в степень alpha
+                if (temp > m_max) { // Если m выходит за пределы, прерываем цикл
+                    break;
+                }
+            }
+            if (temp > m_max) {
+                break;
+            }
+        }
+
+        // Проверка, что m попадает в нужный диапазон
+        if (temp >= min / 2 && temp <= m_max) {
+            m = temp;
+            flag = true;
+        }
+    }
+
+    return m;
+}
+
+int main() {
+    // Решето Эратосфена
+    vector<int> div{2, 3, 5, 7, 11, 13, 17, 19};
+    vector<int> prime;
+    
+    int X = 500;
+
+    for (int i = 2; i <= X; i++){
+        prime.push_back(i);
+    }
+
+    for (int i = 0; i < div.size(); i++) {
+        for (int j = 2; j < prime.size(); j++){
+            prime.erase(remove_if(prime.begin(), prime.end(), [&div, &i](int j) { return j % div[i] == 0 && j != div[i]; }), prime.end());
+        }
+    }
+
+    int bit;
+    map<int, int> res;
+    
+    cout << "Размер в битах: "; 
+    cin >> bit;
+
+    int m = generate_m(prime, bit);
+
+    int n = 2 * m + 1;
+
+    cout << "Полученное n: " << n << endl;
 }
